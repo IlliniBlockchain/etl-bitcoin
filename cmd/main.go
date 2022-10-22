ec2ca0a329a658bda77dab575721f6d0ff4023ba
Alec Chen <alecchendev@gmail.com>
2022-10-22 16:57:20 -0500

    Change main.go to regtest

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/btcsuite/btcd/rpcclient"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world!")
-	// Load username and password from .env
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error occurred loading .env: Err: %s", err)
-	}
-	user := os.Getenv("RPCUSER")
-	pass := os.Getenv("RPCPASS")
-	// fmt.Print(user, pass)
 	// Connect to local bitcoin core RPC server using HTTP POST mode.
 	connCfg := &rpcclient.ConnConfig{
-		Host:         "localhost:8332",
-		User:         user,
-		Pass:         pass,
+		Host:         "localhost:18443",
+		User:         "test",
+		Pass:         "test",
 		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
 		DisableTLS:   true, // Bitcoin core does not provide TLS by default
 	}
-	// Notice the notification parameter is nil since notifications are
-	// not supported in HTTP POST mode.
-	client, err := rpcclient.New(connCfg, nil)
+
+	client, err := rpcclient.NewBatch(connCfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Shutdown()
 
 	// Get the current block count.
-	blockCount, err := client.GetBlockCount()
+	blockCountReq := client.GetBlockCountAsync()
+	client.Send()
+	blockCount, err := blockCountReq.Receive()
 	if err != nil {
 		log.Fatal(err)
 	}
