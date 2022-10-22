db2b52515ab0dea7d5ff481945ef593a72b6489e
Alec Chen <alecchendev@gmail.com>
2022-10-22 11:35:44 -0500

    Add ping test and standard test client helper

--- client/rpc/rpcclient_test.go
+++ client/rpc/rpcclient_test.go
@@ -1,11 +1,24 @@
 package rpcclient
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/stretchr/testify/assert"
 )
+
+func GetTestRPCClient() (*RPCClient, error) {
+	connCfg := &rpcclient.ConnConfig{
+		Host:         "localhost:18443", // regtest
+		User:         "test",
+		Pass:         "test",
+		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
+		DisableTLS:   true, // Bitcoin core does not provide TLS by default
+	}
+	client, err := New(connCfg, nil)
+	return client, err
+}
 
 func TestNewRPCClient(t *testing.T) {
 	connCfg := &rpcclient.ConnConfig{
@@ -17,4 +30,12 @@ func TestNewRPCClient(t *testing.T) {
 	}
 	_, err := New(connCfg, nil)
 	assert.NoError(t, err)
+}
+
+func TestRPCClientPing(t *testing.T) {
+	client, err := GetTestRPCClient()
+	assert.NoError(t, err)
+	block_count, err := client.GetBlockCount()
+	assert.NoError(t, err)
+	fmt.Println(block_count)
 }
