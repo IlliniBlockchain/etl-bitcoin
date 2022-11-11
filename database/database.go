e6b1692ad9aca1acc87cdef0773af7662670421f
Javier Nieto <javiermnieto89@gmail.com>
2022-11-11 00:23:09 -0600

    init database and dbtx interfaces

--- database/database.go
+++ database/database.go
@@ -1,5 +1,26 @@
 package database
 
+import (
+	"context"
+
+	"github.com/IlliniBlockchain/etl-bitcoin/types"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+)
+
 // Database represents a database connection.
 type Database interface {
+	LastBlockhash() (*chainhash.Hash, error)
+	NewDBTx() (DBTx, error)
+	Close() error
 }
+
+type DBTx interface {
+	AddBlockHeader(blockheaders *types.BlockHeader) error
+	AddTransaction(tx *types.Transaction) error
+	Commit() error
+	Rollback() error
+}
+
+type DBConstructor func(ctx context.Context, opts DBOptions) (Database, error)
+
+type DBOptions map[string]interface{}
