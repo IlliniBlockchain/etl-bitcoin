d29760cec3868459fd136fe881445194f156f82b
Javier Nieto <javiermnieto89@gmail.com>
2022-11-11 19:38:34 -0600

    Add comments to database interfaces

--- database/database.go
+++ database/database.go
@@ -9,22 +9,31 @@ import (
 
 // Database represents a database connection.
 type Database interface {
+	// LastBlockhash returns the blockhash of the last block committed to the database.
 	LastBlockhash() (*chainhash.Hash, error)
+	// NewDBTx returns a new DBTx.
 	NewDBTx() (DBTx, error)
+	// Close closes the database connection. Only callable once.
 	Close() error
 }
 
+// DBTx represents a database transaction to make atomic changes to a database.
 type DBTx interface {
+	// AddBlockHeader adds block header data to this database transaction.
 	AddBlockHeader(blockheaders *types.BlockHeader) error
+	// AddTransaction adds transaction data to this database transaction.
 	AddTransaction(tx *types.Transaction) error
+	// Commit commits this database transaction to a database.
 	Commit() error
-	Rollback() error
 }
 
+// DBConstructor represents a function that constructs a database connection.
 type DBConstructor func(ctx context.Context, opts DBOptions) (Database, error)
 
+// DBOptions represents a map of arbitrary options when constructing a database connection.
 type DBOptions map[string]interface{}
 
+// GetOpt returns the value of the option with the given key. If the option is not set, the default value is returned.
 func GetOpt[T any](opts DBOptions, key string, def T) (T, error) {
 	opt, ok := opts[key]
 	if !ok {
