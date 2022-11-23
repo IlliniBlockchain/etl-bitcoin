d1d66d9eec64d56a21a8917ff85222771909582e
Javier Nieto <javiermnieto89@gmail.com>
2022-11-23 11:12:41 -0600

    Handle read msg

--- database/csv/csv.go
+++ database/csv/csv.go
@@ -2,41 +2,38 @@ package csv
 
 import (
 	"context"
-	"encoding/csv"
 	"fmt"
-	"os"
 	"sync"
 
 	"golang.org/x/sync/errgroup"
 )
 
+// CSVRecord is a single row in a CSV file
 type CSVRecord interface {
 	Headers() []string
-	Row() []any
+	Row() []string
 }
 
+// CSVDatabase is a base implementation of a database stored across CSV files.
 type CSVDatabase struct {
 	csv_files  sync.Map
 	maxWorkers int
-
-	msgs chan CSVMsg
-
-	ctx      context.Context
-	cancel   context.CancelFunc
-	stopOnce sync.Once
-	g        *errgroup.Group
+	msgs       chan CSVMsg
+	ctx        context.Context
+	stopOnce   sync.Once
+	g          *errgroup.Group
 }
 
+// NewCSVDatabase creates a new CSVDatabase.
 func NewCSVDatabase(ctx context.Context, filePaths map[string]string, maxWorkers int) (*CSVDatabase, error) {
 	if len(filePaths) == 0 {
 		return nil, fmt.Errorf("no file paths provided")
 	}
-	ctx, cancel := context.WithCancel(ctx)
 	g, ctx := errgroup.WithContext(ctx)
 	db := &CSVDatabase{
 		maxWorkers: maxWorkers,
+		msgs:       make(chan CSVMsg),
 		ctx:        ctx,
-		cancel:     cancel,
 		g:          g,
 	}
 	for key, path := range filePaths {
@@ -51,8 +48,10 @@ func NewCSVDatabase(ctx context.Context, filePaths map[string]string, maxWorkers
 	return db, nil
 }
 
+// Close closes the database.
+//
+// Implements database.Database.
 func (db *CSVDatabase) Close() error {
-	done := false
 	db.stopOnce.Do(func() {
 		close(db.msgs)
 		db.csv_files.Range(func(key, value interface{}) bool {
@@ -65,26 +64,21 @@ func (db *CSVDatabase) Close() error {
 	return db.g.Wait()
 }
 
-func (db *CSVDatabase) SendMsgAsync(msg CSVMsg) error {
-	if db.ctx.Err() != nil {
-		return db.ctx.Err()
-	}
+// SendMsgAsync sends a message to the database asynchronously.
+func (db *CSVDatabase) SendMsgAsync(msg CSVMsg) {
 	db.msgs <- msg
-	return nil
 }
 
+// SendMsg sends a message to the database synchronously.
 func (db *CSVDatabase) SendMsg(msg CSVMsg) error {
-	if err := db.SendMsgAsync(msg); err != nil {
-		return err
-	}
+	db.SendMsgAsync(msg)
 	return msg.Wait()
 }
 
+// SendMsgs sends a batch of messages to the database synchronously.
 func (db *CSVDatabase) SendMsgs(msgs []CSVMsg) error {
 	for _, msg := range msgs {
-		if err := db.SendMsgAsync(msg); err != nil {
-			return err
-		}
+		db.SendMsgAsync(msg)
 	}
 	for _, msg := range msgs {
 		if err := msg.Wait(); err != nil {
@@ -94,22 +88,28 @@ func (db *CSVDatabase) SendMsgs(msgs []CSVMsg) error {
 	return nil
 }
 
-func (db *CSVDatabase) insert(msg *CSVInsertMsg) error {
-	val, ok := db.csv_files.Load(msg.FileKey())
+func (db *CSVDatabase) loadFile(fileKey string) (*csvFile, error) {
+	val, ok := db.csv_files.Load(fileKey)
 	if !ok {
-		return fmt.Errorf("csv file not found for %s", msg.FileKey())
-	}
-	if len(msg.records) == 0 {
-		return nil
+		return nil, fmt.Errorf("csv file not found for %s", fileKey)
 	}
 
 	csvFile := val.(*csvFile)
+	return csvFile, nil
+}
+
+func (db *CSVDatabase) insert(msg *CSVInsertMsg) error {
+	csvFile, err := db.loadFile(msg.FileKey())
+	if err != nil {
+		return err
+	}
+
 	records := make([][]string, 0, len(msg.records)+1)
 	if !csvFile.hasHeader {
 		records = append(records, msg.records[0].Headers())
 	}
 	for _, record := range msg.records {
-		records = append(records, anyRowToString(record.Row()))
+		records = append(records, record.Row())
 	}
 	if err := csvFile.writeAll(records); err != nil {
 		return err
@@ -117,11 +117,23 @@ func (db *CSVDatabase) insert(msg *CSVInsertMsg) error {
 	return nil
 }
 
+func (db *CSVDatabase) read(msg *CSVReadMsg) error {
+	csvFile, err := db.loadFile(msg.FileKey())
+	if err != nil {
+		return err
+	}
+
+	msg.Records, err = csvFile.read(msg.line, msg.limit)
+	return err
+}
+
 func (db *CSVDatabase) csvWorker() error {
 	for msg := range db.msgs {
 		switch msg := msg.(type) {
 		case *CSVInsertMsg:
 			msg.finish(db.insert(msg))
+		case *CSVReadMsg:
+			msg.finish(db.read(msg))
 		default:
 			return fmt.Errorf("unknown message type %T", msg)
 		}
