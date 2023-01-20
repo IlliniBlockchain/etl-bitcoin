4cff4f5c481319624954478a4382bab9db705dac
Javier <javiermnieto89@gmail.com>
2023-01-20 14:15:46 +0000

    Add context to loader run functions

--- loader/loader.go
+++ loader/loader.go
@@ -1,6 +1,11 @@
 package loader
 
 import (
+	"context"
+	"log"
+	"reflect"
+	"runtime"
+
 	"github.com/IlliniBlockchain/etl-bitcoin/client"
 	"github.com/IlliniBlockchain/etl-bitcoin/database"
 )
@@ -13,7 +18,7 @@ type LoaderMsg[T any] struct {
 
 // ILoader is a simple interface for loaders.
 type ILoader interface {
-	Run() error
+	Run(ctx context.Context) error
 }
 
 // Loader is the go to loader for an inidividual stage in the pipeline
@@ -34,7 +39,7 @@ func (loader *Loader[S, D]) Dst() <-chan *LoaderMsg[D] {
 }
 
 func NewLoader[S, D any](client client.Client, src <-chan *LoaderMsg[S], f LoaderFunc[S, D]) *Loader[S, D] {
-	dst := make(chan *LoaderMsg[D])
+	dst := make(chan *LoaderMsg[D], 1)
 	loader := &Loader[S, D]{
 		client,
 		src,
@@ -48,18 +53,21 @@ func NewLoader[S, D any](client client.Client, src <-chan *LoaderMsg[S], f Loade
 // transforms the data and sends it to the next loader.
 // Importantly, when it's src channel is closed, it closes its
 // dst channel, which causes a domino effect of closing loader channels.
-func (loader *Loader[S, D]) Run() error {
+func (loader *Loader[S, D]) Run(ctx context.Context) error {
 	// Close dst to signal to downstream loaders that there is
 	// no more data whenever execution stops.
 	defer close(loader.dst)
 
 	// Listen for messages from upstream loaders.
 	for msg := range loader.src {
+		log.Printf("%s (%s) started\n", runtime.FuncForPC(reflect.ValueOf(loader.f).Pointer()).Name(), msg.dbTx.blockRange)
 		output, err := loader.f(loader.client, msg.data)
+		log.Printf("%s (%s) finished\n", runtime.FuncForPC(reflect.ValueOf(loader.f).Pointer()).Name(), msg.dbTx.blockRange)
 		if err != nil {
 			return err
 		}
 		loader.dst <- &LoaderMsg[D]{msg.dbTx, output}
+		log.Printf("%s (%s) sent\n", runtime.FuncForPC(reflect.ValueOf(loader.f).Pointer()).Name(), msg.dbTx.blockRange)
 	}
 	return nil
 }
@@ -80,8 +88,9 @@ func NewLoaderSink[S any](src <-chan *LoaderMsg[S], f LoaderSinkFunc[S]) *Loader
 	return loader
 }
 
-func (loader *LoaderSink[S]) Run() error {
+func (loader *LoaderSink[S]) Run(ctx context.Context) error {
 	for msg := range loader.src {
+		log.Printf("%s (%s) started\n", runtime.FuncForPC(reflect.ValueOf(loader.f).Pointer()).Name(), msg.dbTx.blockRange)
 		if err := loader.f(msg.dbTx, msg.data); err != nil {
 			return err
 		}
